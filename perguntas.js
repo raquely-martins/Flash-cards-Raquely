2307769d71edd9765dd32f2c73c1f4c4d5bb6d35
raquely martins <145395054+raquely-martins@users.noreply.github.com>
2024-10-24 14:52:02 -0300

    24/10/2024

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'portugues'
-    '1-  Qual é a função de um texto?'
+    'Português',
+    '1-  Qual é a função de um texto?',
     'informar você sobre alguma coisa, por meio de uma mensagem clara e objetiva, pois tem função referencial ou denotativa.'
 )
 
 criaCartao(
-    'portugues'
-    '2- Qual a importância da linguagem de Libras?'
+    'Português',
+    '2- Qual a importância da linguagem de Libras?',
     'é uma forma de garantir a preservação da identidade das pessoas e comunidades surdas. Além disso, contribui para a valorização e reconhecimento da cultura surda.'
 )
 
 criaCartao(
-    'historia'
-    '3-Quem assinou a Lei Áurea e em que ano?'
+    'História',
+    '3-Quem assinou a Lei Áurea e em que ano?',
     ' 13 de maio de 1888, a Princesa Isabel assinou a Lei Áurea.'
 )
 
 criaCartao(
-    'historia'
-    '4-Qual foi o último país a abolir a escravidão?'
+    'História',
+    '4-Qual foi o último país a abolir a escravidão?',
     'O Brasil foi o último país das Américas a abolir a escravidão. '
 )
